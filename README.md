b83e6221fc132d9e5a03e4263e80d44e2997ae73
Fedor Indutny <fedor.indutny@gmail.com>
2013-08-28 17:23:07 +0400

    build: improve build system

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ WIP
 ```
 svn co http://gyp.googlecode.com/svn/trunk build/gyp
 git clone git@github.com:allending/Kiwi.git deps/Kiwi/Kiwi
-CC=`xcrun -find clang` CXX=`xcrun -find clang++` ./gyp_ispdy -f ninja
+./gyp_ispdy -f ninja
 ninja -C out/Debug && ./out/Debug/test-runner
 ```
--- test/runner.m
+++ test/runner.m
@@ -1,8 +1,30 @@
-#import <Kiwi.h>
+#import <Foundation/Foundation.h>
+#import "Kiwi.h"
 #import <SenTestingKit/SenTestingKit.h>
 #import <ispdy.h>
 
-SPEC_BEGIN(iSpdy)
+SPEC_BEGIN(ISpdySpec)
+  describe(@"ISpdy server", ^{
+    __block ISpdy* c;
+    beforeAll(^{
+      c = [[ISpdy alloc] init: kISpdyV2];
+      BOOL r = [c connect:@"localhost" port:3232 secure: NO];
+      [[theValue(r) should] equal:theValue(YES)];
+    });
+
+    afterAll(^{
+      c = nil;
+    });
+
+    context(@"connecting", ^() {
+      it(@"should not fail", ^() {
+        ISpdyRequest* req = [[ISpdyRequest alloc] init: @"POST" url: @"/"];
+        [c send: req];
+        [req writeString: @"hello world"];
+        [req end];
+      });
+    });
+  });
 SPEC_END
 
 int main() {
