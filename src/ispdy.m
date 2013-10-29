49716b2853752d3d03e9ecb29eedc0ed49c6f7a4
Fedor Indutny <fedor.indutny@gmail.com>
2013-10-29 13:02:04 +0400

    ispdy: allow reconnecting from handleErr:

--- src/ispdy.m
+++ src/ispdy.m
@@ -483,13 +483,13 @@ static const NSTimeInterval kConnectTimeout = 30.0;  // 30 seconds
   if (![self close])
     return;
 
+  [self _closeStreams: err];
+  [self _destroyPings: err];
+
   // Fire global error
   [self _delegateDispatch: ^{
     [self.delegate connection: self handleError: err];
   }];
-
-  [self _closeStreams: err];
-  [self _destroyPings: err];
 }
 
 
